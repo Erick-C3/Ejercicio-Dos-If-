dd276bc6777b81503cbec9f79e39d01de55eb24f
Erick <erick.nahuelpaiva.001@gmail.com>
2023-04-26 16:39:58 -0300

    mejora ui ej 2if

--- script.js
+++ script.js
@@ -9,6 +9,11 @@
         - 1 BTC = 13.54 ETH
         - 1 BTC = 39685.40 USD
 
+    Requisitos nuevos:
+        - mejorar la UI con input button
+            - reemplazar los prompt y confirm por algun codigo
+              que sirva para lo mismo
+        - dar una funcionalidad al boton
 */
 
 const CONVERSION_BTC_ETH = 13.54;
@@ -16,15 +21,33 @@ const CONVERSION_BTC_USD = 39685.40;
 
 const CONVERSION_VERIFICADA_DEF = false;
 
-let btcRecibidos = parseFloat(prompt("Ingrese bitcoins a convertir"));
-let conversionEthVerificada = confirm("Desea convertir a eth?");
+const inputBtcRecibidos = document.querySelector("#btc-ingresado");
+const inputEthVerificacion = document.querySelector("#eth-verificacion");
+const inputUsdVerificacion = document.querySelector("#usd-verificacion");
+const pResultadoConversion = document.querySelector("#resultado-conversion");
+/* let btcRecibidos = parseFloat(prompt("Ingrese bitcoins a convertir"));
+let conversionEthVerificada = confirm("Desea convertir a eth?"); */
+let btcRecibidos = "def btc ingresados";
+let conversionEthVerificada = "estado verificado por def eth";
 let conversionUsdVerificada = CONVERSION_VERIFICADA_DEF;
 
-if( conversionEthVerificada ){
-    document.write("Valor ingresado: "+ btcRecibidos + " bitcoins y valor obtenido en conversion es de: "+ (btcRecibidos * CONVERSION_BTC_ETH) + " ETH" );
-}else{
-    conversionUsdVerificada = confirm("Desea convertir a dolar?");
-    if(conversionUsdVerificada){
-        document.write("Valor ingresado: "+ btcRecibidos + " bitcoins y valor obtenido en conversion es de: "+ (btcRecibidos * CONVERSION_BTC_USD) + " USD" );
+function convertir(){
+    /* console.log( inputNumeroBtc.value );
+    console.log( inputEthVerificacion.value);
+    console.log( inputUsdVerificacion.value); */
+    btcRecibidos = inputBtcRecibidos.value;
+    conversionEthVerificada = inputEthVerificacion.value;
+
+    if( conversionEthVerificada == "true" ){
+        //document.write("Valor ingresado: "+ btcRecibidos + " bitcoins y valor obtenido en conversion es de: "+ (btcRecibidos * CONVERSION_BTC_ETH) + " ETH" );
+        pResultadoConversion.innerHTML =  ("Valor ingresado: "+ btcRecibidos + " bitcoins y valor obtenido en conversion es de: "+ (btcRecibidos * CONVERSION_BTC_ETH) + " ETH" );
+    }else{
+        //conversionUsdVerificada = confirm("Desea convertir a dolar?");
+        conversionUsdVerificada = inputUsdVerificacion.value;
+        if(conversionUsdVerificada == "true"){
+            //document.write("Valor ingresado: "+ btcRecibidos + " bitcoins y valor obtenido en conversion es de: "+ (btcRecibidos * CONVERSION_BTC_USD) + " USD" );
+            pResultadoConversion.innerHTML =  ("Valor ingresado: "+ btcRecibidos + " bitcoins y valor obtenido en conversion es de: "+ (btcRecibidos * CONVERSION_BTC_USD) + " USD" );
+        }
     }
 }
+
